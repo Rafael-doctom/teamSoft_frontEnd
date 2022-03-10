e1d362c4bc6c6dd3b5d9d10b4ac2f22e00c37743
Rafael-doctom <dasilva.rafaelpt@gmail.com>
2022-03-10 19:05:35 -0300

    feat: home added

---
+++ src/pages/home/index.js
@@ -0,0 +1,13 @@
+import Main from "../../components/Molecules/main/index";
+import  Navbar  from "../../components/Molecules/navbar/index";
+
+const Home = () => {
+    return (
+        <>
+            <Navbar />
+            <Main />
+        </>
+    )
+};
+
+export default Home;
