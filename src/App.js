c5c03dafa80c881cf4276573242aa91d5de7aa8b
Rafael-doctom <dasilva.rafaelpt@gmail.com>
2022-03-11 11:37:30 -0300

    build application

--- src/App.js
+++ src/App.js
@@ -1,6 +1,4 @@
 import { BrowserRouter } from 'react-router-dom';
-import GlobalStyle from './globalStyle';
-import Home from './pages/home';
 
 import Routes from './routes'
 function App() {
--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -6,7 +6,6 @@ const Home = () => {
     return (
         <>
             <GlobalStyle />
-
             <Navbar />
             <Main />
         </>
